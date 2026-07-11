a2858b3b0bae2eb8af2393b9a57b2dac0aaeea3c
barry818 <879958251@qq.com>
2026-07-11 08:50:14 +0800

    1

--- _worker copy.js
+++ _worker copy.js
@@ -5,7 +5,7 @@ export default {
     
     // 2. 修改目标地址（这里换成你的最终目标）
     // 只需要改 hostname（主机名），保留原有的路径和参数
-    url.hostname = "raw.githubusercontent.com/barry818/TV/refs/heads/main/btv.json";
+    url.hostname = "webhtv-remote-cloudflare.879958251.workers.dev";
     url.protocol = "https:"; // 强制使用 HTTPS
     
     // 3. 转发请求
